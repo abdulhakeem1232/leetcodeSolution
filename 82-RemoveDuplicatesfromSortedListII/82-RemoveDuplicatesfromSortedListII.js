bcb4db057284446a8b9efac0907f8a0ba85e3676
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2024-03-12 14:10:23 +0530

    Create file

---
+++ 82-RemoveDuplicatesfromSortedListII/82-RemoveDuplicatesfromSortedListII.js
@@ -0,0 +1,21 @@
+        head=cur
+        }
+            cur=cur.next
+        while(cur&& cur.val==head.val){
+        let cur=head
+var deleteDuplicates = function(head) {
+    while(head &&head.next && head.val==head.next.val){
+ * @return {ListNode}
+ */
+/**
+ * @param {ListNode} head
+ */
+ * }
+ *     this.next = (next===undefined ? null : next)
+    }
+    let cur=head,prev=head
+    while(cur){
+        let flag=false
+        let next=cur.next
+        while(next && next.val==cur.val){
+[
