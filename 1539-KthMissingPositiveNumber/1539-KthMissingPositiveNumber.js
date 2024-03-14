fa4602c0f61f3f7c0b2b215c875be341f61dc1b7
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2024-03-14 06:17:36 +0530

    Create file

---
+++ 1539-KthMissingPositiveNumber/1539-KthMissingPositiveNumber.js
@@ -0,0 +1,17 @@
+  }
+   if(missing.length<k){
+    let start=arr[arr.length-1]+1
+    }
+        c++
+        i++
+    }else{
+        c++
+    while(missing.length!=k){
+        missing.push(start)
+        start++
+    }
+   }
+   return missing[k-1]
+};
+        missing.push(c)
+[
