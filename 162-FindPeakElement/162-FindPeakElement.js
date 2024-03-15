145b8f6e29393de3b9474e37b7186def79e89a8c
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2024-03-15 06:57:58 +0530

    Create file

---
+++ 162-FindPeakElement/162-FindPeakElement.js
@@ -0,0 +1,11 @@
+var findPeakElement = function(nums) {
+    let max=-Infinity,index=0
+    for(let i=0;i<nums.length;i++){
+        if(nums[i]>max){
+            index=i
+        }
+    }
+    return index
+            max=nums[i]
+};
+[
