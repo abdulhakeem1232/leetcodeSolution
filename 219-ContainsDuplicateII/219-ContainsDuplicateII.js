10de0c350dd5855537e2923fb59d5ca304d0c5c2
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2024-03-19 09:24:17 +0530

    Create file

---
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.js
@@ -0,0 +1,17 @@
+        if(obj.hasOwnProperty([nums[i]])){
+            if(Math.abs(obj[nums[i]]-i)<=k){
+        }
+                return true
+            }
+            obj[nums[i]]=i
+    for(i=0;i<nums.length;i++){
+
+    }
+    let obj={}
+var containsNearbyDuplicate = function(nums, k) {
+ */
+ * @return {boolean}
+ * @param {number} k
+ * @param {number[]} nums
+/**
+[
