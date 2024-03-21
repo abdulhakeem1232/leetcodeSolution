9d1e9a21e36cd967f9a8b0ccada1e46ac2534b5f
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2024-03-21 08:27:49 +0530

    Create file

---
+++ 206-ReverseLinkedList/206-ReverseLinkedList.js
@@ -0,0 +1,16 @@
+ */
+var reverseList = function(head) {
+    let prev= null
+    let cur=head
+    while(cur!=null){
+ * @return {ListNode}
+ * @param {ListNode} head
+/**
+ * }
+ */
+ *     this.next = (next===undefined ? null : next)
+ *     this.val = (val===undefined ? 0 : val)
+ * function ListNode(val, next) {
+ * Definition for singly-linked list.
+/**
+[1,2,3,4,5]
