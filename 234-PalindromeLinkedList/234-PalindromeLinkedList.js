92ea7744715d25720fbeda6760e7815ce16a0828
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2024-03-22 09:04:17 +0530

    Create file

---
+++ 234-PalindromeLinkedList/234-PalindromeLinkedList.js
@@ -0,0 +1,20 @@
+    let arr=[]
+    let curr=head
+    while(curr){
+        arr.push(curr.val)
+        curr=curr.next
+    }
+    curr=head
+var isPalindrome = function(head) {
+ * @return {boolean}
+ */
+ * @param {ListNode} head
+/**
+ */
+ * }
+ *     this.next = (next===undefined ? null : next)
+ *     this.val = (val===undefined ? 0 : val)
+ * function ListNode(val, next) {
+ * Definition for singly-linked list.
+/**
+[1,2,2,1]
