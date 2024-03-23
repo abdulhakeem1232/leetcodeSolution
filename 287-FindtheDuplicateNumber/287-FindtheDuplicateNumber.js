d916229e59cbf34c52ab2bfa969b2949787f12f0
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2024-03-23 12:36:30 +0530

    Create file

---
+++ 287-FindtheDuplicateNumber/287-FindtheDuplicateNumber.js
@@ -0,0 +1,13 @@
+        else{
+        obj[nums[i]]=1
+        }
+        return nums[i]
+        if(!obj.hasOwnProperty(nums[i])){
+    for(let i=0;i<nums.length;i++){
+    let obj={}
+var findDuplicate = function(nums) {
+ */
+ * @return {number}
+        }
+    }
+[
