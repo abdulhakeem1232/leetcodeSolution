ffa329132c97496ccfddcbb79fdc33fc9794e498
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2024-03-29 09:10:16 +0530

    Create file

---
+++ 1721-SwappingNodesinaLinkedList/1721-SwappingNodesinaLinkedList.js
@@ -0,0 +1,17 @@
+    var swap2,swap1
+        while(cur){
+            index++
+            if(index==k){
+ */
+var swapNodes = function(head, k) {
+    let cur=head,index=0,stack=[]
+               swap1=cur
+            }
+            stack.push(cur)
+            cur=cur.next
+        }
+        for(let i=0;i<k-1;i++){
+            stack.pop()
+        }
+         swap2=stack.pop()
+[
