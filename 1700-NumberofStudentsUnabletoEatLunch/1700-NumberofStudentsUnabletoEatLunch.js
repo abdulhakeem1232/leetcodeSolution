ccdd4baa1c7fcbcf53e5be91d7948cd405d4ed2e
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2024-04-08 22:39:37 +0530

    Create file

---
+++ 1700-NumberofStudentsUnabletoEatLunch/1700-NumberofStudentsUnabletoEatLunch.js
@@ -0,0 +1,16 @@
+        } else {
+            students.push(students.shift())
+            unable++
+            if (unable == students.length) {
+                break;
+            }
+        }
+    }
+            unable = 0
+            sandwiches.shift()
+            students.shift()
+        if (students[0] == sandwiches[0]) {
+    while (students.length) {
+    let unable = 0
+    return unable
+[
