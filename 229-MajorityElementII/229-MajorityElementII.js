01ad77c55edfbc31c5339833c9da0d38011feab6
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2024-04-29 20:35:24 +0530

    Create file

---
+++ 229-MajorityElementII/229-MajorityElementII.js
@@ -0,0 +1,15 @@
+           if(nums[i]==nums[j]&& nums[i]!=null){
+               nums[j]=null;
+               c++;
+           }
+       for(let j=i+1;j<nums.length;j++){
+       }
+       let c=1;
+   for(let i=0;i<nums.length;i++){
+    let k=[];
+var majorityElement = function(nums) {
+       if(c>(nums.length/3)){
+           k.push(nums[i]);
+        if(nums[i]==null) continue;
+       }
+[
