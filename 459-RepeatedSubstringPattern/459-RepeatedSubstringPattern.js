54f7a02b3e5c2efb36766601a4c1f86c48485cb5
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2024-05-01 09:46:51 +0530

    Create file

---
+++ 459-RepeatedSubstringPattern/459-RepeatedSubstringPattern.js
@@ -0,0 +1,17 @@
+    if(s.length%i==0){
+        let sub=s.substring(0,i)
+        let value=s.length/i
+       let dum = sub.repeat(value);
+        if(dum==s)return true
+    }
+   for(let i=1;i<=s.length/2;i++){
+    if(s.length==2) return s[0]==s[1]
+var repeatedSubstringPattern = function(s) {
+ */
+ * @return {boolean}
+    if (s.length <= 1) return false;
+   }
+   return false
+       if (dum == s) return true;
+};
+"
