0fa6579a205da4761c8e9df87ef7f2262dd16a3d
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2024-05-04 10:02:46 +0530

    Create file

---
+++ 2621-Sleep/2621-Sleep.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    return new Promise((res,rej)=>{
+}
+
+/** 
+ * let t = Date.now()
+        setTimeout(()=>{
+    })
+            res(`Function delayed for ${millis}`)
+        },millis)
+1
