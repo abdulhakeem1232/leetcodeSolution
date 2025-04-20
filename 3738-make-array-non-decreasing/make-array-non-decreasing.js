7a069cee93f3db16bfe91ad335423573d3171c47
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2025-04-20 23:22:49 +0530

    Time: 4 ms (63.89%) | Memory: 74.7 MB (47.22%) - LeetSync

--- 3738-make-array-non-decreasing/make-array-non-decreasing.js
+++ 3738-make-array-non-decreasing/make-array-non-decreasing.js
@@ -3,15 +3,11 @@
  * @return {number}
  */
 var maximumPossibleSize = function(nums) {
-    let stack = [];
-    for (let i = 0; i < nums.length; i++) {
-        // Push current element
-        stack.push(nums[i]);
-
-        // If it's not non-decreasing, remove the last added (i.e., nums[i])
-        if (stack.length >= 2 && stack[stack.length - 2] > stack[stack.length - 1]) {
-            stack.pop(); // Remove nums[i]
-        }
+    let stack=[]
+    for(let i=0;i<nums.length;i++){
+        stack.push(nums[i])
+        if(stack.length>=2 && stack[stack.length-1]<stack[stack.length-2])
+         stack.pop()
     }
-    return stack.length;
+    return stack.length
 };
