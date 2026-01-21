b009c001a504f25db377a1c5d639f1ef3c823dcc
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2026-01-21 11:07:48 +0300

    Time: 1 ms (45.45%) | Memory: 57.8 MB (9.09%) - LeetSync

---
+++ 4009-bitwise-or-of-even-numbers-in-an-array/bitwise-or-of-even-numbers-in-an-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var evenNumberBitwiseORs = function(nums) {
+    let ans=0
+    for(let i=0;i<nums.length;i++)
+        if(nums[i]%2==0) ans=(ans|nums[i])
+    return ans
+};  
