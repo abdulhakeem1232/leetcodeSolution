1b2ff00eb8f112073127c5fe976892dc1032b78b
abdulhakeem1232 <143874378+abdulhakeem1232@users.noreply.github.com>
2026-03-29 15:43:18 +0300

    Time: 0 ms (100.00%) | Memory: 55.8 MB (71.43%) - LeetSync

---
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @return {boolean}
+ */
+var canBeEqual = function(s1, s2) {
+    if(s1==s2)return true
+    if(s1[2]+s1[1]+s1[0]+s1[3]==s2) return true
+    if(s1[0]+s1[3]+s1[2]+s1[1]==s2) return true
+    if(s1[2]+s1[3]+s1[0]+s1[1]==s2) return true
+    return false
+};
